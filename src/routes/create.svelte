fe08028d4506565f4abec0d9f1d4c9f340a1b0ef
Tytus Planck <tytusplanck@Tytuss-MacBook-Pro.local>
2020-09-24 17:51:05 -0400

    Initial Svelte App

---
+++ src/routes/create.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let dogName;
+	let dogBreed;
+
+	function create() {
+		console.log({'dogName': this.dogName, 'dogBreed': this.dogBreed});
+	}
+</script>
+
+<svelte:head>
+	<title>Create</title>
+</svelte:head>
+
+<h1>Let's create a new Dog for You!</h1>
+
+<form>
+    <div>
+      <input bind:value={dogName} type="text" name="dogname" placeholder="Dog's Name" />
+    </div>
+    <div>
+      <input bind:value={dogBreed} type="text" name="breed" placeholder="Dog Breed" />
+    </div>
+    <div>
+      <button on:click={create}>Create Dog</button>
+    </div>
+  </form>
